2f71b66ff7c683b3ecd5e763d1301463b4fe0311
Jason Kölker <jason@koelker.net>
2011-05-30 23:12:18 -0500

    start the implementation

--- elasticmail/imapserver.py
+++ elasticmail/imapserver.py
@@ -1,25 +1,95 @@
+import urllib
+import urlparse
+
+import anyjson
+
+from twisted.cred import portal
+from twisted.internet import defer
 from twisted.mail import imap4
+from twisted.web import client
 from zope.interface import implements
 
 
-MAILBOXDELIMITER = '.'
+MAILBOXDELIMITER = '/'
+ES_URL = "http://192.168.56.101:9200"
+
+def getPageFactory(url, contextFactory=None, *args, **kwargs):
+    return client._makeGetterFactory(url, client.HTTPClientFactory,
+                                     contextFactory, *args, **kwargs)
+
+
+class ElasticSearchError(Exception):
+    pass
 
 
 class ImapUserAccount(object):
     implements(imap4.IAccount, imap4.INamespacePresenter)
 
-    def __init__(self, user):
-        self.user = user
+    def __init__(self, owner):
+        self.owner = owner
+        self.user self.domain = owner.split('@')
+
+    def _setAlias(self, mbox, name):
+        pass
 
     def addMailbox(self, name, mbox=None):
         if mbox:
-            # Uhh what do we do
-            pass
+            return self._setAlias(mbox, name)
         else:
             return self.create(name)
 
     def create(self, pathspec):
-        pass
+        pathspec = pathspec.strip(MAILBOXDELIMITER)
+
+        d = self.ensure_path(pathspec)
+        return d
+            
+    def ensureMailboxConfig(self, path):
+        mailbox = path.split(MAILBOXDELIMITER)[-1]
+        url = '/'.join([ES_URL, self.user, "config", path])
+        user, domain = self.user.split('@')
+
+        data = { "uid_validity": self.newValidityUID(),
+                 "owner": self.owner,
+                 "user": self.user,
+                 "domain": self.domain,
+                 "aliases": [],
+                 "path": path,
+                 "mailbox": mailbox
+                 "readers": [self.owner],
+                 "writers": [self.owner],
+                 }
+        factory = getPageFactory(url, method="PUT", postdata=data) 
+
+        def success(body):
+            res = anyjson.deserialize(body)
+
+        def fail(error):
+            raise imap4.MailboxException()
+
+        factory.deferred.addCallbacks(callback=success, errback=fail)
+        return factory.deferred
+
+    def ensure_index(self, user):
+        url = '/'.join([ES_URL, user])
+        factory = getPageFactory(url, method="PUT")
+
+        def success(body):
+            res = anyjson.deserialize(body)
+
+            if int(factory.status) != 200:
+                raise ElasticSearchError(res.get("error"))
+            elif not res.get("ok"):
+                raise ElasticSearchError(body)
+
+            return (imap4.IAccount, ImapUserAccount(user), lambda: None)
+        
+        factory.deferred.addCallback(success)
+        return factory.deferred
+        
+    def requestAvatar(self, avatarId, mind, *interfaces):
+        if imap4.IAccount in interfaces:
+            return self.ensure_index(avatarId)
 
     def select(self, name, rw=True):
         pass
@@ -53,7 +123,7 @@ class ImapUserAccount(object):
 
 
 class ElasticMessage(object):
-    implements(imap4.IMessage)
+    implements(imap4.IMessage, imap4.IMessageFile)
 
     def getHeaders(self, negate, *names):
         pass
@@ -77,6 +147,9 @@ class ElasticMessage(object):
         pass
 
     def getInternalDate(self):
+        pass
+
+    def open(self):
         pass
 
 
@@ -141,3 +214,28 @@ class ElasticMailbox(object):
     def search(query, uid):
         pass
 
+
+class ImapUserRealm(object):
+    implements(portal.IRealm)
+
+    def ensureIndex(self, user):
+        url = '/'.join([ES_URL, user])
+        factory = getPageFactory(url, method="PUT")
+
+        def success(body):
+            res = anyjson.deserialize(body)
+
+            if int(factory.status) != 200:
+                raise ElasticSearchError(res.get("error"))
+            elif not res.get("ok"):
+                raise ElasticSearchError(body)
+
+            return (imap4.IAccount, ImapUserAccount(user), lambda: None)
+        
+        factory.deferred.addCallback(success)
+        return factory.deferred
+        
+    def requestAvatar(self, avatarId, mind, *interfaces):
+        if imap4.IAccount in interfaces:
+            return self.ensureIndex(avatarId)
+        raise KeyError("None of the requested interfaces is supported")
